5b2b8ee43f0008ed2e731253f3f3fc1933d8a446
Ahmed Saber <ahmed0saber33@gmail.com>
2023-08-05 10:03:22 +0300

    Allow using different `database model` locally

--- models/subscriberSchema.js
+++ models/subscriberSchema.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
+const dotenv = require('dotenv')
+dotenv.config()
+const SUBSCRIBERS_MODEL = process.env.SUBSCRIBERS_MODEL
 
 const subscriberSchema = new Schema({
     email: String,
 })
 
-const Subscriber = mongoose.model('Subscriber', subscriberSchema)
+const Subscriber = mongoose.model(SUBSCRIBERS_MODEL, subscriberSchema)
 
 module.exports = Subscriber
