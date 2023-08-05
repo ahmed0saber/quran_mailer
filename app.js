b21974ce4ac5e19ad1124df2e5ca722b14f999c6
BasemYahia402 <basmyhy496@gmail.com>
2023-08-05 18:14:18 +0300

    solve the Subscribing more than one time

--- app.js
+++ app.js
@@ -104,20 +104,34 @@ const sendContactEmail = (name, email, message) => {
 }
 
 const addNewSubscriber = (email) => {
-    const currentUser = {
-        email: removeHtmlTags(email),
-        isValid: true,
-    }
+  const processedEmail = removeHtmlTags(email);
 
-    const subscriber = new Subscriber(currentUser)
-    subscriber.save()
-        .then((result) => {
-            console.log(result)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-}
+  // Check if the email already exists in the database
+  Subscriber.findOne({ email: processedEmail })
+    .then((subscriber) => {
+      if (subscriber) {
+        // Subscriber already exists, handle accordingly (e.g., update their details, show an error message, etc.)
+        console.log('Subscriber already exists:', subscriber);
+      } else {
+        // Subscriber does not exist, add them to the database
+        const newSubscriber = new Subscriber({
+          email: processedEmail,
+          isValid: true,
+        });
+
+        newSubscriber.save()
+          .then((result) => {
+            console.log('New subscriber added:', result);
+          })
+          .catch((err) => {
+            console.log('Error while adding new subscriber:', err);
+          });
+      }
+    })
+    .catch((err) => {
+      console.log('Error while checking subscriber existence:', err);
+    });
+};
 
 const removeHtmlTags = (str) => {
     return str.replace(/<\/?\w+>/g, "")
