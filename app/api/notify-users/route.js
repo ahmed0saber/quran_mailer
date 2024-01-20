6638a44aed195a675640533d5ea83bae0127db21
Ahmed Saber <ahmed0saber33@gmail.com>
2024-01-21 01:50:15 +0200

    Set `maxDuration = 10`

--- app/api/notify-users/route.js
+++ app/api/notify-users/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"
 import clientPromise from "@/lib/mongodb"
 
-export const maxDuration = 60
+export const maxDuration = 10
 
 const nodemailer = require('nodemailer')
 
