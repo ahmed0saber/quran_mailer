09bde5e46ef5369df87c02043c0c5da76ab1c347
Ahmed Saber <ahmed0saber33@gmail.com>
2024-05-14 12:39:13 +0300

    Reduce middleware invocations

--- middleware.js
+++ middleware.js
@@ -1,11 +1,10 @@
-const routesRequiringAuth = ['/api/admin/login', '/api/admin/logs']
-
 export function middleware(request) {
-    const pathname = request.nextUrl.pathname
-    if (routesRequiringAuth.includes(pathname)) {
-        const authHeader = request.headers.get('authorization')
-        if (authHeader !== `Basic ${process.env.ADMIN_USERNAME}:${process.env.ADMIN_PASSWORD}`) {
-            return new Response('Unauthorized', { status: 401 })
-        }
+    const authHeader = request.headers.get('authorization')
+    if (authHeader !== `Basic ${process.env.ADMIN_USERNAME}:${process.env.ADMIN_PASSWORD}`) {
+        return new Response('Unauthorized', { status: 401 })
     }
+}
+
+export const config = {
+    matcher: ['/api/admin/login', '/api/admin/logs'],
 }
